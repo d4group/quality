44b66b84a7d48ef3ccb80a2fde37b68ecfd77894
Tomasz Diakowski <biuro.d4@gmail.com>
2024-05-02 09:23:29 +0200

    add images

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
   <v-app>
-    <v-layout>
-      <ToolBar />
-    </v-layout>
-
+    <ToolBar />
     <v-main>
       <HelloWorld />
       <v-container fluid>
-		<v-row>
-			<News />
-		</v-row>
-        <v-row justify="center">
-          <v-col cols="4" md="3"><AICard /></v-col>
-          <v-col cols="4" md="3"><EnglishCard /></v-col>
-          <v-col cols="4" md="3"><ItCard /></v-col>
-          <v-col cols="4" md="3"><CoursesCard /></v-col>
+        <v-row>
+          <News />
         </v-row>
-
+        <v-row justify="center">
+          <v-col cols="4" md="3">
+            <AICard />
+          </v-col>
+          <v-col cols="4" md="3">
+            <EnglishCard />
+          </v-col>
+          <v-col cols="4" md="3">
+            <ItCard />
+          </v-col>
+          <v-col cols="4" md="3">
+            <CoursesCard />
+          </v-col>
+        </v-row>
         <v-row justify="center">
           <v-col cols="12" md="4">
             <h1>Proszę o kontakt</h1>
@@ -26,8 +30,8 @@
       </v-container>
     </v-main>
     <v-footer>
-		<Footer />
-	</v-footer>
+      <Footer />
+    </v-footer>
   </v-app>
 </template>
 
@@ -36,9 +40,9 @@ import HelloWorld from "./components/HelloWorld.vue";
 import ToolBar from "./components/ToolBar.vue";
 import News from "./components/News.vue";
 import EnglishCard from "./components/EnglishCard.vue";
+import AICard from "./components/AICard.vue";
 import ItCard from "./components/ItCard.vue";
 import CoursesCard from "./components/CoursesCard.vue";
-import AICard from "./components/AICard.vue";
 import ContactForm from "./components/ContactForm.vue";
-import Footer from "./components/Footer.vue"
+import Footer from "./components/Footer.vue";
 </script>
--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import App from './App.vue'
 const app = createApp(App)
 
 // Vuetify
-import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
