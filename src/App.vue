469f9259a8e6023b3ab506a12aaaebbd902284f5
Tomasz Diakowski <biuro.d4@gmail.com>
2024-05-18 14:49:03 +0200

    xxx

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,6 @@
     <v-main>
       <HelloWorld />
       <v-container>
-		<v-row justify="center">
-			<v-col cols="12" xs="12" md="12" xl="12">
-				<About />
-			</v-col>
-        </v-row>
         <v-row justify="center">
 			<v-col cols="12" xs="12" md="12" xl="12">
 				<News />
@@ -28,15 +23,10 @@
             <CoursesCard />
           </v-col>
         </v-row>
-		<v-row justify="center">
-			<v-col cols="12" xs="12" md="12" xl="12">
-				<AIoffer />
-			</v-col>
-		</v-row>
         <v-row justify="center">
 			<v-col xs="12" md="12" xl="12">
             <h1>Proszę o kontakt</h1>
-            <Contact />
+            <ContactForm />
           </v-col>
         </v-row>
       </v-container>
@@ -44,23 +34,17 @@
     <v-footer>
       <Footer />
     </v-footer>
-	<div>
-		<Cookies />
-	</div>
   </v-app>
 </template>
 
 <script setup>
 import HelloWorld from "./components/HelloWorld.vue";
 import ToolBar from "./components/ToolBar.vue";
-import About from "./components/About.vue";
 import News from "./components/News.vue";
 import EnglishCard from "./components/EnglishCard.vue";
 import AICard from "./components/AICard.vue";
 import ItCard from "./components/ItCard.vue";
 import CoursesCard from "./components/CoursesCard.vue";
-import Contact from "./components/Contact.vue";
+import ContactForm from "./components/ContactForm.vue";
 import Footer from "./components/Footer.vue";
-import AIoffer from "./components/AIoffer.vue";
-import Cookies from "./components/Cookies.vue";
 </script>
